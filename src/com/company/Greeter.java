84795673904b2f3fb79b1b20e5a2398f683ea1ee
Ruslan <mutygullin@yandexlyceum.ru>
2022-06-02 14:04:13 +0300

    added greet() and greet(name) to Greeter

--- src/com/company/Greeter.java
+++ src/com/company/Greeter.java
@@ -4,4 +4,8 @@ public class Greeter {
     public static void greet(){
         System.out.println("Hello world!");
     }
+
+    public static void greet(String name){
+        System.out.println("Hello " + name);
+    }
 }
