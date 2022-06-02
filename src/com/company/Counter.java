1159b17c4d30a8a27be0e4be0d92cf9bdc879c1a
Ruslan <mutygullin@yandexlyceum.ru>
2022-06-02 13:39:10 +0300

    added Counter

---
+++ src/com/company/Counter.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Counter {
+    int count;
+
+    public Counter() {
+        this.count = 0;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	 System.out.println("Hello World!");
-	 System.out.println("How are you?");
+	 System.out.println("How are you?!");
+     Counter c = new Counter();
+     System.out.println(++c.count);
     }
 }
